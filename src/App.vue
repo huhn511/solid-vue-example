be83edfb1c8e3d20126a80d3e43f2e76f10e96be
huhn <sehe89@gmail.com>
2019-05-14 23:37:03 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Solid Vue Components Demo"/>
+    <LoginButton v-on:onSuccess="handleLogin" v-if="!session" />
+    <LogoutButton v-on:onSuccess="handleLogout" v-if="session" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { LoginButton, LogoutButton } from 'solid-vue-components'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld, LoginButton, LogoutButton
+  },
+  data() {
+    return {
+      session: false
+    }
+  },
+  methods: {
+    handleLogin(session) {
+      console.log("logged in!")
+      this.session = session
+    },
+    handleLogout() {
+      this.session = false
+    }
   }
 }
 </script>
@@ -24,5 +41,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  border-radius: 10px ;
+  padding: 15px 30px;
+  color: #42b983
+}
+button:hover {
+  cursor: pointer;
+  transform: scale(1.1)
 }
 </style>
